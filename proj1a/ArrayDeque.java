1a4411a72769fe452d8c5c97fbf2ab5b8a620c0b
Alice <jxliualice@gmail.com>
2024-06-25 15:20:43 -0600

    Array 3.0

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,29 +3,35 @@ public class ArrayDeque<T> {
   private int size;
   private int nextFirst;
   private int nextLast;
+  private int start;
 
   public ArrayDeque() {
     items = (T[]) new Object[8];
     size = 0;
     nextFirst = 0;
     nextLast = 0;
+    start = 0;
   }
 
-  private void resize(int capacity) {
-    T[] a = (T[]) new Object[capacity];
-    int head = nextFirst + 1;
-    int tail = nextLast - 1;
-    int j = 0;
-    for (int i = 0; i < size; i++) {
-      if (head < size) {
-        a[i] = items[head];
-        head += 1;
-      } else {
-        a[i] = items[j];
-        j += 1;
+  public void resize(int capacity) {
+    if (capacity > size) {
+      T[] a = (T[]) new Object[capacity];
+      int head = nextFirst + 1;
+      int tail = nextLast - 1;
+      int j = 0;
+      for (int i = 0; i < size; i++) {
+        if (head < size) {
+          a[i] = items[head];
+          head += 1;
+        } else {
+          a[i] = items[j];
+          j += 1;
+        }
       }
+      nextLast = size;
+      nextFirst = capacity - 1;
+      items = a;
     }
-    items = a;
   }
 
   /* Adds an item of type T to the front of the deque. */
@@ -36,7 +42,6 @@ public class ArrayDeque<T> {
       nextLast += 1;
     } else if (size == items.length) {
       resize(size * 2);
-      nextFirst = size * 2 - 1;
       items[nextFirst] = item;
       nextFirst -= 1;
     } else {
@@ -54,10 +59,9 @@ public class ArrayDeque<T> {
   public void addLast(T item) {
     if (size == items.length) {
       resize(size * 2);
-
       items[size] = item;
       size += 1;
-      nextLast = size;
+      nextLast += 1;
     } else {
       items[nextLast] = item;
       size += 1;
@@ -79,34 +83,101 @@ public class ArrayDeque<T> {
   }
 
   /* Prints the items in the deque from first to last, separated by a space. */
-  // public void printDeque() {
-
-  // }
+  public void printDeque() {
+    if (size == 0) {
+      System.out.print("null");
+    } else {
+      int head = nextFirst + 1;
+      int tail = nextLast - 1;
+      while (head < items.length) {
+        System.out.print(items[head] + " ");
+        head += 1;
+      }
+      for (int j = 0; j <= tail; j++) {
+        System.out.print(items[j] + " ");
+      }
+    }
+  }
 
   /*
    * Removes and returns the item at the front of the deque. If no such item
    * exists, returns null.
    */
-  // public T removeFirst() {
-  // }
+  public T removeFirst() {
+    if (size == 0) {
+      return null;
+    }
+    int head = nextFirst + 1;
+    if (head < items.length) {
+      nextFirst += 1;
+      T ele = items[head];
+      items[head] = null;
+      size -= 1;
+      return ele;
+    } else if (head == items.length) {
+      head = start;
+      size -= 1;
+      T ele = items[head];
+      items[head] = null;
+      nextFirst = start;
+      return ele;
+    }
+    return null;
+  }
 
   /*
    * Removes and returns the item at the back of the deque. If no such item
    * exists, returns null.
    */
-  // public T removeLast() {
-
-  // }
+  public T removeLast() {
+    if (size == 0) {
+      return null;
+    }
+    int tail = nextLast - 1;
+    if (tail >= 0) {
+      T ele = items[tail];
+      items[tail] = null;
+      nextLast -= 1;
+      size -= 1;
+      return ele;
+    } else if (tail == -1) {
+      tail = items.length - 1;
+      size -= 1;
+      T ele = items[tail];
+      items[tail] = null;
+      nextLast = items.length - 1;
+      return ele;
+    }
+    return null;
+  }
 
   /*
    * Gets the item at the given index, where 0 is the front, 1 is the next item,
    * and so forth. If no such item exists, returns null. Must not alter the deque
    */
-  // public T get(int index) {
-
-  // }
-
-  // public static void main(String[] args) {
-  // ArrayDeque<String> A = new ArrayDeque<>();
-  // }
+  public T get(int index) {
+    if (size == 0) {
+      return null;
+    } else {
+      return items[index];
+    }
+  }
 }
+
+// public static void main(String[] args) {
+// ArrayDeque<String> L = new ArrayDeque<>();
+// L.addFirst("1");
+// L.addLast("2");
+// L.addFirst("3");
+// L.addFirst("4");
+// L.addLast("5");
+// L.addFirst("6");
+// L.removeFirst();
+// L.removeFirst();
+// L.removeFirst();
+// L.removeFirst();
+// L.removeFirst();
+// L.removeFirst();
+// L.removeFirst();
+// L.removeFirst();
+// }
